94bff940aebc9da2b6beb00df52e0605be9b8d4d
alexiscyber14 <142sledgehammer14@gmail.com>
2023-06-12 18:30:11 +0100

    fix linters

--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class LikeTest < ActiveSupport::TestCase
-  test "should update post likes_counter after creating a like" do
-    user = User.create(name: "John Doe", post_counter: 0)
-    post = Post.create(title: "Test Post", comments_counter: 0, likes_counter: 0, author: user)
+  test 'should update post likes_counter after creating a like' do
+    user = User.create(name: 'John Doe', post_counter: 0)
+    post = Post.create(title: 'Test Post', comments_counter: 0, likes_counter: 0, author: user)
 
     assert_difference('post.likes_counter', 1) do
-      Like.create(author: user, post: post)
+      Like.create(author: user, post:)
     end
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "should not save user without name" do
+  test 'should not save user without name' do
     user = User.new
-    assert_not user.save, "Saved the user without a name"
+    assert_not user.save, 'Saved the user without a name'
   end
 
-  test "should save user with valid attributes" do
-    user = User.new(name: "John Doe", post_counter: 0)
-    assert user.save, "Could not save the user with valid attributes"
+  test 'should save user with valid attributes' do
+    user = User.new(name: 'John Doe', post_counter: 0)
+    assert user.save, 'Could not save the user with valid attributes'
   end
 end
