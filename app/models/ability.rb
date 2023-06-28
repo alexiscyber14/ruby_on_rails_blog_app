8b159cbcc533c24298bb9e7d64b528af8b7d3e38
alexiscyber14 <142sledgehammer14@gmail.com>
2023-06-28 08:51:13 +0100

    fix lin error

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,7 @@ class Ability
   def initialize(user)
     can :read, [Post, Comment]
     return unless user.present?
+
     can :manage, [Post, Comment], author: user
 
     return unless user.admin?
