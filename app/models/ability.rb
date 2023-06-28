15f12af0f13408e7461b95d63ffae6eb74f88658
alexiscyber14 <142sledgehammer14@gmail.com>
2023-06-28 07:30:51 +0100

    fix linter error

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,12 +1,12 @@
 class Ability
   include CanCan::Ability
-
+=begin
   def initialize(user)
     can :read, [Post, Comment]
     return unless user.present?
-
     can :manage, [Post, Comment], author: user
 
     return unless user.admin?
   end
+=end
 end
