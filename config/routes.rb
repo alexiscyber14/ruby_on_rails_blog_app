a4cd0f554dbacf4625388bc52c5551d4f078737f
alexiscyber14 <142sledgehammer14@gmail.com>
2023-06-29 08:21:33 +0100

    add upadte

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     end
   end
 
-  namespace :api do
+  namespace :api, defaults:{ format: :json } do
     namespace :v1 do
       resources :users, only: [:index, :show] do
         resources :posts, only: [:index, :show] do
